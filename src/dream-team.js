e97e36c4de13f00cc3f7e472d645fec8ed6de48a
TatsianaTarasova <70853063+TatsianaTarasova@users.noreply.github.com>
2021-02-09 11:50:50 +0300

    feat: solution dream

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(Array.isArray(members)){
+   let str = [];
+   for (let member of members){
+   if (typeof member === 'string') {
+   let arrMem = member.toUpperCase().trim();
+   str.push(arrMem[0]);   
+   }
+  }return str.sort().join('');
+}else{
+  return false;
+}
 };
